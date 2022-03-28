ec45e4001c6d60db1354361f7256d4bc5df8c89c
vagner da silva <vagnrdaia@gmail.com>
2022-03-27 23:05:31 -0300

    cartItemsAmount reduce qtde product on cart

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -25,10 +25,16 @@ const Home = (): JSX.Element => {
   const [products, setProducts] = useState<ProductFormatted[]>([]);
   const { addProduct, cart } = useCart();
 
-  // const cartItemsAmount = cart.reduce((sumAmount, product) => {
-  //   // TODO
-  // }, {} as CartItemsAmount)
+  const cartItemsAmount = cart.reduce((sumAmount, product) => {
 
+
+    return {
+      ...sumAmount,
+      [product.id]:product.amount
+    }
+  }, {} as CartItemsAmount)
+
+  console.log(cartItemsAmount)
   useEffect(() => {
     async function loadProducts() {
       await api.get('products')
@@ -39,12 +45,7 @@ const Home = (): JSX.Element => {
   }, []);
 
   function handleAddProduct(id: number) {
-    cart.map(product => {
-      if(product.id === id) {
-        
-      }
-    })
-    addProduct(id)
+    addProduct(id);
     // TODO
   }
 
@@ -62,7 +63,7 @@ const Home = (): JSX.Element => {
           >
             <div data-testid="cart-product-quantity">
               <MdAddShoppingCart size={16} color="#FFF" />
-              {/* {cartItemsAmount[product.id] || 0} */} 2
+              {cartItemsAmount[product.id] || 0}
             </div>
 
             <span>ADICIONAR AO CARRINHO</span>
